fce721cd4c890977c2205a51507af7d8b627eca4
Taha Malaldır <tevfikhanmalaldir@gmail.com>
2020-09-27 01:52:09 +0300

    Loading Added

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,58 +1,63 @@
-<template
-  ><div class="work">
-    <b-row><h1 class="text">Work</h1></b-row>
-    <b-row class="row">
-      <a
-        v-for="user in users"
-        :key="user.id"
-        :href="user.html_url"
-        target="blank"
+<template>
+  <div class="work">
+    <div v-if="isLoading">
+      <h1 class="text">Loading...</h1>
+    </div>
+    <div v-else>
+      <b-row><h1 class="text">Works</h1></b-row>
+      <b-row class="row">
+        <a
+          v-for="user in users"
+          :key="user.id"
+          :href="user.html_url"
+          target="blank"
+        >
+          <b-card-group v-if="!user.fork" class="group"
+            ><b-card class="text-center">
+              <b-card-title>{{ user.name }}</b-card-title>
+              <b-card-text>{{ user.description }}</b-card-text>
+              <b-card-footer>
+                <span v-if="user.language"
+                  ><i class="fas fa-code"></i>&nbsp;
+                </span>
+                {{ user.language }}
+                <span><i class="fas fa-star"></i>&nbsp; </span
+                >{{ user.stargazers_count }}
+                <span><i class="fas fa-code-branch"></i>&nbsp; </span
+                >{{ user.forks_count }}
+              </b-card-footer>
+            </b-card></b-card-group
+          ></a
+        ></b-row
       >
-        <b-card-group v-if="!user.fork" class="group"
-          ><b-card class="text-center">
-            <b-card-title>{{ user.name }}</b-card-title>
-            <b-card-text>{{ user.description }}</b-card-text>
-            <b-card-footer>
-              <span v-if="user.language"
-                ><i class="fas fa-code"></i>&nbsp;
-              </span>
-              {{ user.language }}
-              <span><i class="fas fa-star"></i>&nbsp; </span
-              >{{ user.stargazers_count }}
-              <span><i class="fas fa-code-branch"></i>&nbsp; </span
-              >{{ user.forks_count }}
-            </b-card-footer>
-          </b-card></b-card-group
-        ></a
-      ></b-row
-    >
-    <b-row><h1 class="text">Fork</h1></b-row>
-    <b-row>
-      <a
-        v-for="user in users"
-        :key="user.id"
-        :href="user.html_url"
-        target="blank"
-      >
-        <b-card-group v-if="user.fork" class="group"
-          ><b-card class="text-center">
-            <b-card-title>{{ user.name }}</b-card-title>
-            <b-card-text>{{ user.description }}</b-card-text>
+      <b-row><h1 class="text">Forks</h1></b-row>
+      <b-row>
+        <a
+          v-for="user in users"
+          :key="user.id"
+          :href="user.html_url"
+          target="blank"
+        >
+          <b-card-group v-if="user.fork" class="group"
+            ><b-card class="text-center">
+              <b-card-title>{{ user.name }}</b-card-title>
+              <b-card-text>{{ user.description }}</b-card-text>
 
-            <b-card-footer>
-              <span v-if="user.language"
-                ><i class="fas fa-code"></i>&nbsp;
-              </span>
-              {{ user.language }}
-              <span><i class="fas fa-star"></i>&nbsp; </span
-              >{{ user.stargazers_count }}
-              <span><i class="fas fa-code-branch"></i>&nbsp; </span
-              >{{ user.forks_count }}
-            </b-card-footer>
-          </b-card></b-card-group
-        ></a
-      ></b-row
-    >
+              <b-card-footer>
+                <span v-if="user.language"
+                  ><i class="fas fa-code"></i>&nbsp;
+                </span>
+                {{ user.language }}
+                <span><i class="fas fa-star"></i>&nbsp; </span
+                >{{ user.stargazers_count }}
+                <span><i class="fas fa-code-branch"></i>&nbsp; </span
+                >{{ user.forks_count }}
+              </b-card-footer>
+            </b-card></b-card-group
+          ></a
+        ></b-row
+      >
+    </div>
   </div>
 </template>
 
@@ -60,7 +65,8 @@
 export default {
   data() {
     return {
-      users: []
+      users: [],
+      isLoading: true
     }
   },
   name: 'Main',
@@ -69,16 +75,15 @@ export default {
       .then(response => response.json())
       .then(data => {
         this.users = data
+        this.isLoading = false
       })
-    console.log(this.users)
   }
 }
 </script>
 
 <style>
 .work {
-  padding-right: 20px;
-  padding-top: 100px;
+  padding: 100px 10px 200px 10px;
 }
 .text {
   font-size: 4rem;
@@ -91,7 +96,7 @@ export default {
   float: left;
 }
 .card {
-  width: 30rem;
+  width: 29.3rem;
   height: auto;
   display: block;
   top: 0px;
